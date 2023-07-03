3d80281d9deadec13f20601c7189646d77ed4534
Mattia Formichetti <mattiaformichetti@gmail.com>
2023-07-04 00:48:05 +0200

    Dinamicizzazione del Modello di Registrazione

--- pages/account/register.js
+++ pages/account/register.js
@@ -28,7 +28,7 @@ export default function SignUp() {
 
   return (
     <ThemeProvider theme={theme}>
-      <Container component="main" maxWidth="xs">
+      <Container component="main" maxWidth="md">
         <CssBaseline />
         <Box
           sx={{
@@ -51,121 +51,167 @@ export default function SignUp() {
             sx={{ mt: 3 }}
           >
             <Grid container spacing={2}>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  id="codiceFiscale"
-                  label="Codice Fiscale"
-                  name="codiceFiscale"
-                  autoComplete="codiceFiscale"
-                />
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  autoComplete="given-name"
-                  name="firstName"
-                  required
-                  fullWidth
-                  id="firstName"
-                  label="Nome"
-                  autoFocus
-                />
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  required
-                  fullWidth
-                  id="lastName"
-                  label="Cognome"
-                  name="lastName"
-                  autoComplete="family-name"
-                />
+              <Grid container item spacing={2} sm={12} md={6}>
+                <Grid item xs={12}>
+                  <TextField
+                    required
+                    fullWidth
+                    id="codiceFiscale"
+                    label="Codice Fiscale"
+                    name="codiceFiscale"
+                    autoComplete="codiceFiscale"
+                  />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    autoComplete="given-name"
+                    name="firstName"
+                    required
+                    fullWidth
+                    id="firstName"
+                    label="Nome"
+                    autoFocus
+                  />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    required
+                    fullWidth
+                    id="lastName"
+                    label="Cognome"
+                    name="lastName"
+                    autoComplete="family-name"
+                  />
+                </Grid>
+
+                <Grid item xs={12}>
+                  <FormControl required>
+                    <FormLabel id="sesso">Sesso</FormLabel>
+                    <RadioGroup
+                      aria-labelledby="sesso"
+                      defaultValue="female"
+                      name="sesso"
+                      row
+                    >
+                      <FormControlLabel
+                        value="femmina"
+                        control={<Radio />}
+                        label="Femmina"
+                      />
+                      <FormControlLabel
+                        value="maschio"
+                        control={<Radio />}
+                        label="Maschio"
+                      />
+                      <FormControlLabel
+                        value="altro"
+                        control={<Radio />}
+                        label="Altro"
+                      />
+                    </RadioGroup>
+                  </FormControl>
+                </Grid>
+                <Grid item xs={12}>
+                  <LocalizationProvider
+                    dateAdapter={AdapterDayjs}
+                    adapterLocale="it"
+                  >
+                    <DatePicker
+                      required
+                      label="Data di Nascita"
+                      sx={{
+                        width: "100%",
+                      }}
+                    />
+                  </LocalizationProvider>
+                </Grid>
+                <Grid item xs={12} sm={8}>
+                  <TextField
+                    required
+                    fullWidth
+                    id="luogoNascita"
+                    label="Luogo di Nascita"
+                    name="luogoNascita"
+                    autoComplete="luogoNascita"
+                  />
+                </Grid>
+                <Grid item xs={12} sm={4}>
+                  <TextField
+                    required
+                    fullWidth
+                    id="provinciaNascita"
+                    label="Provincia"
+                    name="provinciaNascita"
+                    autoComplete="provinciaNascita"
+                  />
+                </Grid>
               </Grid>
 
-              <Grid item xs={12}>
-                <FormControl required>
-                  <FormLabel id="sesso">Sesso</FormLabel>
-                  <RadioGroup
-                    aria-labelledby="sesso"
-                    defaultValue="female"
-                    name="sesso"
-                    row
-                  >
-                    <FormControlLabel
-                      value="femmina"
-                      control={<Radio />}
-                      label="Femmina"
-                    />
-                    <FormControlLabel
-                      value="maschio"
-                      control={<Radio />}
-                      label="Maschio"
-                    />
-                    <FormControlLabel
-                      value="altro"
-                      control={<Radio />}
-                      label="Altro"
-                    />
-                  </RadioGroup>
-                </FormControl>
-              </Grid>
-              <Grid item xs={12}>
-                <LocalizationProvider
-                  dateAdapter={AdapterDayjs}
-                  adapterLocale="it"
-                >
-                  <DatePicker
+              <Grid container item spacing={2} sm={12} md={6}>
+                <Grid item xs={12}>
+                  <TextField
                     required
-                    label="Data di Nascita"
-                    sx={{
-                      width: "100%",
-                    }}
+                    fullWidth
+                    id="indirizzo"
+                    label="Indirizzo"
+                    name="indirizzo"
+                    autoComplete="indirizzo"
                   />
-                </LocalizationProvider>
+                </Grid>
+                <Grid item xs={12} sm={8}>
+                  <TextField
+                    required
+                    fullWidth
+                    id="città"
+                    label="Città"
+                    name="città"
+                    autoComplete="città"
+                  />
+                </Grid>
+                <Grid item xs={12} sm={4}>
+                  <TextField
+                    required
+                    fullWidth
+                    id="cap"
+                    label="CAP"
+                    name="cap"
+                    autoComplete="cap"
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    required
+                    fullWidth
+                    id="provincia"
+                    label="Provincia"
+                    name="provincia"
+                    autoComplete="provincia"
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    required
+                    fullWidth
+                    id="email"
+                    label="Indirizzo Email"
+                    name="email"
+                    autoComplete="email"
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <MuiTelInput
+                    sx={{ width: "100%" }}
+                    defaultCountry="it"
+                    value="+39"
+                  />
+                </Grid>
               </Grid>
-              <Grid item xs={12} sm={8}>
-                <TextField
-                  required
-                  fullWidth
-                  id="luogoNascita"
-                  label="Luogo di Nascita"
-                  name="luogoNascita"
-                  autoComplete="luogoNascita"
-                />
-              </Grid>
-              <Grid item xs={12} sm={4}>
-                <TextField
-                  required
-                  fullWidth
-                  id="provinciaNascita"
-                  label="Provincia"
-                  name="provinciaNascita"
-                  autoComplete="provinciaNascita"
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  id="email"
-                  label="Indirizzo Email"
-                  name="email"
-                  autoComplete="email"
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <MuiTelInput
-                  sx={{ width: "100%" }}
-                  defaultCountry="it"
-                  value="+39"
-                />
-              </Grid>
+
               <Grid item xs={12}>
                 <FormControlLabel
                   required
                   control={<Checkbox value="accettaPrivacy" color="primary" />}
-                  label="Iscrivendoti dichiari di aver preso visione dell'informativa sulla Privacy"
+                  label="Iscrivendoti dichiari di aver preso visione dell'Informativa sulla Privacy"
                 />
               </Grid>
             </Grid>
