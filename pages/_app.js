fe06a8b87409e8f1c91e7c3dec435e46a2653883
Mattia Formichetti <mattiaformichetti@gmail.com>
2023-08-08 16:24:55 +0200

    Mass Reformat

--- pages/_app.js
+++ pages/_app.js
@@ -3,86 +3,83 @@ import React from "react";
 import PropTypes from "prop-types";
 import Head from "next/head";
 import CssBaseline from "@mui/material/CssBaseline";
-import {CacheProvider} from "@emotion/react";
-import {lightTheme, darkTheme} from "../src/components/theme/theme";
+import { CacheProvider } from "@emotion/react";
+import { lightTheme, darkTheme } from "../src/components/theme/theme";
 import createEmotionCache from "../src/components/utils/createEmotionCache";
-import {ThemeProvider as CustomThemeProvider} from "../src/components/theme/ThemeContext";
-import {ThemeProvider} from "@mui/material/styles";
+import { ThemeProvider as CustomThemeProvider } from "../src/components/theme/ThemeContext";
+import { ThemeProvider } from "@mui/material/styles";
 import LoadingOverlay from "../src/components/utils/LoadingOverlay";
 import ThemeColorListener from "../src/components/theme/ThemeColorListener"; // Importa il componente
-import {AlertMeProvider} from "../src/components/layout/alert/AlertMeContext";
+import { AlertMeProvider } from "../src/components/layout/alert/AlertMeContext";
 
 //REDUX ---
-import {wrapper} from "/src/store/store";
-import {useSelector} from "react-redux";
+import { wrapper } from "/src/store/store";
+import { useSelector } from "react-redux";
 
 const clientSideEmotionCache = createEmotionCache();
 
 function MyApp(props) {
-	const {Component, emotionCache = clientSideEmotionCache, pageProps} = props;
-	const [themeMode, setThemeMode] = React.useState("light");
-	const isLoading = useSelector((state) => state.loading);
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+  const [themeMode, setThemeMode] = React.useState("light");
+  const isLoading = useSelector((state) => state.loading);
 
-	const [autoMode, setAutoMode] = React.useState("false");
+  const [autoMode, setAutoMode] = React.useState("false");
 
-	React.useEffect(() => {
-		if (typeof window !== "undefined" && window.localStorage) {
-			const savedThemeMode = localStorage.getItem("themeMode");
-			setAutoMode(localStorage.getItem("autoMode"));
-			if (savedThemeMode) {
-				setThemeMode(savedThemeMode);
-			}
-		}
-	}, []);
+  React.useEffect(() => {
+    if (typeof window !== "undefined" && window.localStorage) {
+      const savedThemeMode = localStorage.getItem("themeMode");
+      setAutoMode(localStorage.getItem("autoMode"));
+      if (savedThemeMode) {
+        setThemeMode(savedThemeMode);
+      }
+    }
+  }, []);
 
-	const toggleThemeMode = (newThemeMode) => {
-		//modificabile tramite context
-		if (typeof window !== "undefined" && window.localStorage) {
-			console.log("APP TOGGLE THEME MODE", themeMode);
-			localStorage.setItem("themeMode", newThemeMode);
-			setThemeMode(newThemeMode);
-			setAutoMode(localStorage.getItem("autoMode"));
-		}
-	};
+  const toggleThemeMode = (newThemeMode) => {
+    //modificabile tramite context
+    if (typeof window !== "undefined" && window.localStorage) {
+      console.log("APP TOGGLE THEME MODE", themeMode);
+      localStorage.setItem("themeMode", newThemeMode);
+      setThemeMode(newThemeMode);
+      setAutoMode(localStorage.getItem("autoMode"));
+    }
+  };
 
-	const appTheme = React.useMemo(() => {
-		return {
-			...(themeMode === "dark" ? darkTheme : lightTheme),
-			palette: {
-				...(themeMode === "dark" ? darkTheme : lightTheme).palette,
-				mode: themeMode,
-			},
-		};
-	}, [themeMode]);
+  const appTheme = React.useMemo(() => {
+    return {
+      ...(themeMode === "dark" ? darkTheme : lightTheme),
+      palette: {
+        ...(themeMode === "dark" ? darkTheme : lightTheme).palette,
+        mode: themeMode,
+      },
+    };
+  }, [themeMode]);
 
-	return (
-		<>
-			<CacheProvider value={emotionCache}>
-				<ThemeProvider theme={appTheme}>
-					<AlertMeProvider>
-						<Head>
-							<meta
-								name='viewport'
-								content='initial-scale=1, width=device-width'
-							/>
-						</Head>
-						<CustomThemeProvider toggleThemeMode={toggleThemeMode}>
-							{autoMode === "true" ? <ThemeColorListener setThemeMode={setThemeMode} /> : <></>}
-							<CssBaseline />
-							{isLoading && <LoadingOverlay />}
-							<Component {...pageProps} />
-						</CustomThemeProvider>
-					</AlertMeProvider>
-				</ThemeProvider>
-			</CacheProvider>
-		</>
-	);
+  return (
+    <>
+      <CacheProvider value={emotionCache}>
+        <ThemeProvider theme={appTheme}>
+          <AlertMeProvider>
+            <Head>
+              <meta name="viewport" content="initial-scale=1, width=device-width" />
+            </Head>
+            <CustomThemeProvider toggleThemeMode={toggleThemeMode}>
+              {autoMode === "true" ? <ThemeColorListener setThemeMode={setThemeMode} /> : <></>}
+              <CssBaseline />
+              {isLoading && <LoadingOverlay />}
+              <Component {...pageProps} />
+            </CustomThemeProvider>
+          </AlertMeProvider>
+        </ThemeProvider>
+      </CacheProvider>
+    </>
+  );
 }
 //const wrapper = createWrapper(store);
 export default wrapper.withRedux(MyApp);
 
 MyApp.propTypes = {
-	Component: PropTypes.elementType.isRequired,
-	emotionCache: PropTypes.object,
-	pageProps: PropTypes.object.isRequired,
+  Component: PropTypes.elementType.isRequired,
+  emotionCache: PropTypes.object,
+  pageProps: PropTypes.object.isRequired,
 };
--- pages/index.js
+++ pages/index.js
@@ -1,276 +1,269 @@
 // Index.js
 import * as React from "react";
 import Router from "next/router";
-import {useSpring, animated, easings} from "@react-spring/web";
-import {useTheme, styled} from "@mui/material/styles";
+import { useSpring, animated, easings } from "@react-spring/web";
+import { useTheme, styled } from "@mui/material/styles";
 import Image from "next/image";
-import {Lato, Poppins} from "next/font/google";
+import { Lato, Poppins } from "next/font/google";
 
 export const lato = Lato({
-	weight: ["300", "400"],
-	subsets: ["latin"],
-	display: "swap",
-	fallback: ["Helvetica", "Arial", "sans-serif"],
+  weight: ["300", "400"],
+  subsets: ["latin"],
+  display: "swap",
+  fallback: ["Helvetica", "Arial", "sans-serif"],
 });
 
 export const poppins = Poppins({
-	weight: ["300", "400", "800"],
-	subsets: ["latin"],
-	display: "swap",
-	fallback: ["Helvetica", "Arial", "sans-serif"],
+  weight: ["300", "400", "800"],
+  subsets: ["latin"],
+  display: "swap",
+  fallback: ["Helvetica", "Arial", "sans-serif"],
 });
 
 const Index = () => {
-	const theme = useTheme();
-	console.log("theme: ", theme);
+  const theme = useTheme();
+  console.log("theme: ", theme);
 
-	// setTimeout(() => {
-	// 	Router.push("/account/login");
-	// }, 8000);
+  // setTimeout(() => {
+  // 	Router.push("/account/login");
+  // }, 8000);
 
-	const Container = styled("div")({
-		width: "100%",
-		height: "100vh",
-		background: theme.palette.background.default,
-		display: "flex",
-		justifyContent: "center",
-		alignItems: "center",
-	});
+  const Container = styled("div")({
+    width: "100%",
+    height: "100vh",
+    background: theme.palette.background.default,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  });
 
-	const Box = styled("div")({
-		width: 250,
-		height: 250,
-		position: "relative",
-		display: "flex",
-		justifyContent: "center",
-		flexDirection: "column",
-	});
+  const Box = styled("div")({
+    width: 250,
+    height: 250,
+    position: "relative",
+    display: "flex",
+    justifyContent: "center",
+    flexDirection: "column",
+  });
 
-	const Title = styled("div")({
-		width: "100%",
-		position: "relative",
-		display: "flex",
-		alignItems: "center",
-		height: 50,
-		justifyContent: "center",
-	});
+  const Title = styled("div")({
+    width: "100%",
+    position: "relative",
+    display: "flex",
+    alignItems: "center",
+    height: 50,
+    justifyContent: "center",
+  });
 
-	const Block = styled(animated.div)({
-		//blocco colore prima riga
-		width: 0,
-		height: 20,
-		background: theme.palette.secondary.main,
-		position: "absolute",
-		display: "flex",
-		justifyContent: "center",
-	});
+  const Block = styled(animated.div)({
+    //blocco colore prima riga
+    width: 0,
+    height: 20,
+    background: theme.palette.secondary.main,
+    position: "absolute",
+    display: "flex",
+    justifyContent: "center",
+  });
 
-	const H1 = styled(animated.h1)({
-		//blocco colore Testo prima riga
-		fontFamily: poppins,
-		color: theme.palette.text.primary,
-		fontSize: 32,
-		display: "flex",
-		alignItems: "baseline",
-		position: "relative",
-		justifyContent: "center",
-	});
+  const H1 = styled(animated.h1)({
+    //blocco colore Testo prima riga
+    fontFamily: poppins,
+    color: theme.palette.text.primary,
+    fontSize: 32,
+    display: "flex",
+    alignItems: "baseline",
+    position: "relative",
+    justifyContent: "center",
+  });
 
-	const Span = styled(animated.span)({
-		width: 0,
-		height: 0,
-		borderRadius: "50%",
+  const Span = styled(animated.span)({
+    width: 0,
+    height: 0,
+    borderRadius: "50%",
 
-		marginLeft: 5,
-		marginTop: -10,
-		position: "absolute",
-		bottom: 13,
-		right: -12,
-	});
+    marginLeft: 5,
+    marginTop: -10,
+    position: "absolute",
+    bottom: 13,
+    right: -12,
+  });
 
-	const Role = styled("div")({
-		width: "100%",
-		position: "relative",
-		display: "flex",
-		alignItems: "center",
-		height: 30,
-		marginTop: -10,
-		justifyContent: "center",
-	});
+  const Role = styled("div")({
+    width: "100%",
+    position: "relative",
+    display: "flex",
+    alignItems: "center",
+    height: 30,
+    marginTop: -10,
+    justifyContent: "center",
+  });
 
-	const SecBlock = styled(animated.div)({
-		//blocco colore seconda riga
-		width: 0,
-		height: 20,
-		background: theme.palette.primary.main,
-		position: "absolute",
-		display: "flex",
-		justifyContent: "center",
-	});
+  const SecBlock = styled(animated.div)({
+    //blocco colore seconda riga
+    width: 0,
+    height: 20,
+    background: theme.palette.primary.main,
+    position: "absolute",
+    display: "flex",
+    justifyContent: "center",
+  });
 
-	const P = styled(animated.p)({
-		//blocco testo colore seconda riga
-		fontWeight: 400,
-		fontFamily: lato,
-		color: theme.palette.text.primary,
-		fontSize: 12,
-		textTransform: "uppercase",
-		letterSpacing: 5,
-	});
+  const P = styled(animated.p)({
+    //blocco testo colore seconda riga
+    fontWeight: 400,
+    fontFamily: lato,
+    color: theme.palette.text.primary,
+    fontSize: 12,
+    textTransform: "uppercase",
+    letterSpacing: 5,
+  });
 
-	const mainBlockAnimation = useSpring({
-		width: "0%",
-		left: "0",
-		from: {width: "0%", left: "0"},
-		to: async (next) => {
-			await next({width: "100%", left: "0"});
-			await next({width: "0", left: "100%"});
-		},
-		config: {
-			duration: 1000,
-			easing: easings.easeInBack,
-		},
-	});
+  const mainBlockAnimation = useSpring({
+    width: "0%",
+    left: "0",
+    from: { width: "0%", left: "0" },
+    to: async (next) => {
+      await next({ width: "100%", left: "0" });
+      await next({ width: "0", left: "100%" });
+    },
+    config: {
+      duration: 1000,
+      easing: easings.easeInBack,
+    },
+  });
 
-	const secBlockAnimation = useSpring({
-		width: "0%",
-		left: "0",
-		from: {width: "0%", left: "0"},
-		to: async (next) => {
-			await next({width: "100%", left: "0"});
-			await next({width: "0", left: "100%"});
-		},
-		config: {
-			duration: 1000,
-			easing: easings.easeInBack,
-		},
-		delay: 2000,
-	});
+  const secBlockAnimation = useSpring({
+    width: "0%",
+    left: "0",
+    from: { width: "0%", left: "0" },
+    to: async (next) => {
+      await next({ width: "100%", left: "0" });
+      await next({ width: "0", left: "100%" });
+    },
+    config: {
+      duration: 1000,
+      easing: easings.easeInBack,
+    },
+    delay: 2000,
+  });
 
-	const mainFadeInAnimation = useSpring({
-		opacity: 0,
-		from: {opacity: 0},
-		to: {opacity: 1},
-		config: {
-			duration: 2000,
-		},
-		delay: 2000,
-	});
+  const mainFadeInAnimation = useSpring({
+    opacity: 0,
+    from: { opacity: 0 },
+    to: { opacity: 1 },
+    config: {
+      duration: 2000,
+    },
+    delay: 2000,
+  });
 
-	const popInAnimation = useSpring({
-		from: {
-			width: 0,
-			height: 0,
-			background: theme.palette.secondary.main,
-			border: "0px solid #ddd",
-			opacity: 0,
-			bottom: 0,
-		},
-		to: async (next) => {
-			await next({
-				width: 10,
-				height: 10,
-				background: theme.palette.secondary.main,
-				opacity: 1,
-				bottom: 45,
-			});
-			await next({width: 7, height: 7, bottom: 0, width: 15});
-			await next({width: 10, height: 10, bottom: 20});
-			await next({
-				width: 7,
-				height: 7,
-				background: theme.palette.secondary.main,
-				border: "0px solid #222",
-				bottom: 13,
-			});
-		},
-		config: {
-			duration: 300,
-		},
-		delay: 3000,
-	});
+  const popInAnimation = useSpring({
+    from: {
+      width: 0,
+      height: 0,
+      background: theme.palette.secondary.main,
+      border: "0px solid #ddd",
+      opacity: 0,
+      bottom: 0,
+    },
+    to: async (next) => {
+      await next({
+        width: 10,
+        height: 10,
+        background: theme.palette.secondary.main,
+        opacity: 1,
+        bottom: 45,
+      });
+      await next({ width: 7, height: 7, bottom: 0, width: 15 });
+      await next({ width: 10, height: 10, bottom: 20 });
+      await next({
+        width: 7,
+        height: 7,
+        background: theme.palette.secondary.main,
+        border: "0px solid #222",
+        bottom: 13,
+      });
+    },
+    config: {
+      duration: 300,
+    },
+    delay: 3000,
+  });
 
-	const secFadeInAnimation = useSpring({
-		opacity: 0,
-		from: {opacity: 0},
-		to: {opacity: 0.5},
-		config: {
-			duration: 2000,
-		},
-		delay: 3200,
-	});
+  const secFadeInAnimation = useSpring({
+    opacity: 0,
+    from: { opacity: 0 },
+    to: { opacity: 0.5 },
+    config: {
+      duration: 2000,
+    },
+    delay: 3200,
+  });
 
-	const allxEndBoxAnimation = useSpring({
-		opacity: 1,
-		from: {opacity: 1},
-		to: {opacity: 0},
-		config: {
-			duration: 1000,
-			easing: easings.easeInBack,
-		},
-		delay: 6000,
-		onRest: () => {
-			Router.push("/account/login");
-		},
-	});
+  const allxEndBoxAnimation = useSpring({
+    opacity: 1,
+    from: { opacity: 1 },
+    to: { opacity: 0 },
+    config: {
+      duration: 1000,
+      easing: easings.easeInBack,
+    },
+    delay: 6000,
+    onRest: () => {
+      Router.push("/account/login");
+    },
+  });
 
-	return (
-		<animated.div
-			style={{
-				width: "100%",
-				height: "100vh",
-				background: "#232323",
-				display: "flex",
-				justifyContent: "center",
-				alignItems: "center",
-				...allxEndBoxAnimation,
-			}}
-		>
-			<Container>
-				<Box
-					style={{
-						display: "flex",
-						flexDirection: "column",
-						alignItems: "center",
-						margin: "auto",
-						marginTop: "90px", // Imposta un valore negativo per alzare il contenuto più in alto
-					}}
-				>
-					<Image
-						src='/images/LogoQ.png'
-						alt='Logo'
-						fill={true}
-						sizes='max-width: 300px'
-						style={{objectFit: "contain"}}
-						priority={true}
-					/>
+  return (
+    <animated.div
+      style={{
+        width: "100%",
+        height: "100vh",
+        background: "#232323",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        ...allxEndBoxAnimation,
+      }}
+    >
+      <Container>
+        <Box
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            margin: "auto",
+            marginTop: "90px", // Imposta un valore negativo per alzare il contenuto più in alto
+          }}
+        >
+          <Image src="/images/LogoQ.png" alt="Logo" fill={true} sizes="max-width: 300px" style={{ objectFit: "contain" }} priority={true} />
 
-					<Box
-						style={{
-							position: "absolute",
-							top: "70%", // Imposta il top al 100% per posizionare il testo sotto l'immagine
-							left: "50%",
-							transform: "translate(-50%, 20px)", // Regola il valore del traslato in base all'altezza desiderata tra l'immagine e il testo
-							textAlign: "center",
-						}}
-					>
-						{/* Inserisci qui i tuoi componenti animati di testo */}
-						<Title>
-							<Block style={mainBlockAnimation} />
-							<H1 style={mainFadeInAnimation}>
-								E-commerce
-								<Span style={popInAnimation} />
-							</H1>
-						</Title>
-						<Role>
-							<SecBlock style={secBlockAnimation} />
-							<P style={secFadeInAnimation}>Powered by Tommys</P>
-						</Role>
-					</Box>
-				</Box>
-			</Container>
-		</animated.div>
-	);
+          <Box
+            style={{
+              position: "absolute",
+              top: "70%", // Imposta il top al 100% per posizionare il testo sotto l'immagine
+              left: "50%",
+              transform: "translate(-50%, 20px)", // Regola il valore del traslato in base all'altezza desiderata tra l'immagine e il testo
+              textAlign: "center",
+            }}
+          >
+            {/* Inserisci qui i tuoi componenti animati di testo */}
+            <Title>
+              <Block style={mainBlockAnimation} />
+              <H1 style={mainFadeInAnimation}>
+                E-commerce
+                <Span style={popInAnimation} />
+              </H1>
+            </Title>
+            <Role>
+              <SecBlock style={secBlockAnimation} />
+              <P style={secFadeInAnimation}>Powered by Tommys</P>
+            </Role>
+          </Box>
+        </Box>
+      </Container>
+    </animated.div>
+  );
 };
 
 export default Index;
