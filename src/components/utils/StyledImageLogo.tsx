8c96f1e98e1e4fad5e0251d18ff9c81833865a9f
Mattia Formichetti <mattiaformichetti@gmail.com>
2023-09-16 22:19:51 +0200

    Initial Subscriptions

---
+++ src/components/utils/StyledImageLogo.tsx
@@ -0,0 +1,9 @@
+import styled from "@emotion/styled";
+import Image from "next/image";
+
+const StyledImageLogo = styled(Image)({
+	padding: "10px",
+	maxWidth: 300,
+});
+
+export default StyledImageLogo;
