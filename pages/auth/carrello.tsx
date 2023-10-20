eab6b2c9817ce28659c8cd4fdedf36ff505184f6
Mattia Formichetti <mattiaformichetti@gmail.com>
2023-10-20 15:42:17 +0200

    Add Initial Screen for Empty Cart

--- pages/auth/carrello.tsx
+++ pages/auth/carrello.tsx
@@ -7,6 +7,7 @@ import { loadStripe } from "@stripe/stripe-js";
 import {
 	Button,
 	IconButton,
+	Link,
 	List,
 	ListItem,
 	ListItemText,
@@ -41,6 +42,7 @@ import {
 } from "@mui/icons-material";
 import { removeFromCart } from "src/components/product/ProductCard";
 import { getPrice, getPrices } from "./store";
+import Router from "next/router";
 
 const Carrello = () => {
 	const { showAlert } = useAlertMe();
@@ -50,6 +52,8 @@ const Carrello = () => {
 	const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
 
 	const user = cart.at(0);
+
+	const isCartEmpty = user ? user.cart.length === 0 : true ? true : false;
 
 	const [stripeSecretKey, setStripeSecretKey] = React.useState(null);
 
@@ -212,260 +216,295 @@ const Carrello = () => {
 			>
 				<AlertMe />
 				<Container>
-					<Typography variant="h4">
-						<strong>Il tuo carrello</strong>
-					</Typography>
-					<List>
-						{user ? (
-							user.cart.map((abbonamento) => {
-								const prices = getPrices(abbonamento);
+					{isCartEmpty ? (
+						<Box
+							textAlign={"center"}
+							marginTop={12}
+						>
+							<Typography
+								variant="h4"
+								gutterBottom
+							>
+								<strong>Il Carrello è vuoto</strong>
+							</Typography>
+							<Typography variant="h5">
+								Per Aggiungere Prodotti visita gli{" "}
+								<Link
+									onClick={() => Router.push("/auth/store")}
+									sx={{
+										mt: 2,
+										textAlign: "center",
+										cursor: "pointer",
+										color: (theme) =>
+											theme.palette.mode === "light" ? "black" : "white",
+									}}
+								>
+									Abbonamenti
+								</Link>
+							</Typography>
+						</Box>
+					) : (
+						<>
+							<Typography variant="h4">
+								<strong>Il tuo carrello</strong>
+							</Typography>
+							<List>
+								{user ? (
+									user.cart.map((abbonamento) => {
+										const prices = getPrices(abbonamento);
 
-								const sconti = (
-									<Stack direction={isMobile ? "row" : "column"}>
-										{abbonamento.convenzione.isConv ? (
-											<Typography
-												marginBottom={3}
-												variant="h5"
-											>
-												<Tooltip
-													title={
-														<span
-															style={{
-																display: "flex",
-																flexDirection: "column",
-															}}
-														>
-															<Typography
-																textAlign={"center"}
-																variant="subtitle2"
-															>
-																Convenzione
-															</Typography>
-															<Typography variant="subtitle2">
-																{abbonamento.convenzione.descConve}
-															</Typography>
-														</span>
-													}
-												>
-													<IconButton>
-														<Handshake color="success" />
-													</IconButton>
-												</Tooltip>
-											</Typography>
-										) : (
-											<></>
-										)}
-										{abbonamento.promozione.isPromo ? (
-											<Typography
-												marginBottom={3}
-												variant="h5"
-											>
-												<Tooltip
-													title={
-														<span
-															style={{
-																display: "flex",
-																flexDirection: "column",
-															}}
-														>
-															<Typography
-																textAlign={"center"}
-																variant="subtitle2"
-															>
-																Promozione
-															</Typography>
-															<Typography variant="subtitle2">
-																{abbonamento.promozione.descPromo}
-															</Typography>
-														</span>
-													}
-												>
-													<IconButton>
-														<Discount color="success" />
-													</IconButton>
-												</Tooltip>
-											</Typography>
-										) : (
-											<></>
-										)}
-										{abbonamento.sceltaOrari.isOrari ? (
-											<Typography
-												marginBottom={3}
-												variant="h5"
-											>
-												<Tooltip
-													title={
-														<span
-															style={{
-																display: "flex",
-																flexDirection: "column",
-															}}
-														>
-															<Typography
-																textAlign={"center"}
-																variant="subtitle2"
-															>
-																Orario Configurabile <br />
-															</Typography>
-															<Typography variant="h6">
-																{`${abbonamento.sceltaOrari.daOrari} - ${abbonamento.sceltaOrari.aOrari}`}
-															</Typography>
-														</span>
-													}
-												>
-													<IconButton>
-														<EditCalendar />
-													</IconButton>
-												</Tooltip>
-											</Typography>
-										) : (
-											<></>
-										)}
-									</Stack>
-								);
-
-								return (
-									<ListItem>
-										<ListItemText>
-											<Stack
-												alignItems={"center"}
-												spacing={2}
-												direction={isMobile ? "column" : "row"}
-											>
-												<Image
-													src={abbonamento.immagine ? abbonamento.immagine : ""}
-													alt={abbonamento.nome}
-													width={125}
-													height={125}
-													style={{ borderRadius: 5 }}
-												/>
-												<Stack spacing={2}>
-													<Box width={"200px"}>
-														<Typography variant="h6">
-															{abbonamento.nome}
-														</Typography>
-														<Typography>{abbonamento.descrizione}</Typography>
-													</Box>
-													{prices.discountedPrice ? (
-														<Stack
-															direction={"row"}
-															spacing={2}
-														>
-															<Typography
-																variant="h6"
-																textAlign={"center"}
-																color={"green"}
-															>
-																{prices.discountedPrice}€
-															</Typography>
-															<Typography
-																variant="h6"
-																textAlign={"center"}
-																color={"grey"}
-																style={{
-																	position: "relative",
-																}}
-															>
-																{prices.basePrice}€
+										const sconti = (
+											<Stack direction={"row"}>
+												{abbonamento.convenzione.isConv ? (
+													<Typography
+														marginBottom={3}
+														variant="h5"
+													>
+														<Tooltip
+															title={
 																<span
 																	style={{
-																		position: "absolute",
-																		top: "50%",
-																		left: "50%",
-																		transform:
-																			"translate(-50%, -50%) rotate(-20deg)",
-																		background: "red",
-																		width: "100%",
-																		height: "2px",
+																		display: "flex",
+																		flexDirection: "column",
 																	}}
-																></span>
-															</Typography>
-														</Stack>
-													) : (
-														<Typography variant="h6">
-															<strong>{prices.basePrice}€</strong>
-														</Typography>
-													)}
-													{isMobile ? sconti : <></>}
-												</Stack>
-												{!isMobile ? sconti : <></>}
+																>
+																	<Typography
+																		textAlign={"center"}
+																		variant="subtitle2"
+																	>
+																		Convenzione
+																	</Typography>
+																	<Typography variant="subtitle2">
+																		{abbonamento.convenzione.descConve}
+																	</Typography>
+																</span>
+															}
+														>
+															<IconButton>
+																<Handshake color="success" />
+															</IconButton>
+														</Tooltip>
+													</Typography>
+												) : (
+													<></>
+												)}
+												{abbonamento.promozione.isPromo ? (
+													<Typography
+														marginBottom={3}
+														variant="h5"
+													>
+														<Tooltip
+															title={
+																<span
+																	style={{
+																		display: "flex",
+																		flexDirection: "column",
+																	}}
+																>
+																	<Typography
+																		textAlign={"center"}
+																		variant="subtitle2"
+																	>
+																		Promozione
+																	</Typography>
+																	<Typography variant="subtitle2">
+																		{abbonamento.promozione.descPromo}
+																	</Typography>
+																</span>
+															}
+														>
+															<IconButton>
+																<Discount color="success" />
+															</IconButton>
+														</Tooltip>
+													</Typography>
+												) : (
+													<></>
+												)}
+												{abbonamento.sceltaOrari.isOrari ? (
+													<Typography
+														marginBottom={3}
+														variant="h5"
+													>
+														<Tooltip
+															title={
+																<span
+																	style={{
+																		display: "flex",
+																		flexDirection: "column",
+																	}}
+																>
+																	<Typography
+																		textAlign={"center"}
+																		variant="subtitle2"
+																	>
+																		Orario Configurabile <br />
+																	</Typography>
+																	<Typography variant="h6">
+																		{`${abbonamento.sceltaOrari.daOrari} - ${abbonamento.sceltaOrari.aOrari}`}
+																	</Typography>
+																</span>
+															}
+														>
+															<IconButton>
+																<EditCalendar />
+															</IconButton>
+														</Tooltip>
+													</Typography>
+												) : (
+													<></>
+												)}
 											</Stack>
-										</ListItemText>
-										<IconButton
-											edge="end"
-											aria-label="delete"
-											sx={{
-												marginRight: "auto",
-												color: theme.palette.error.main,
-											}}
-											onClick={() =>
-												removeFromCart(abbonamento, cart, dispatch)
-											}
-										>
-											<DeleteRounded />
-										</IconButton>
-									</ListItem>
-								);
-							})
-						) : (
-							<></>
-						)}
-					</List>
-					<Stack
-						direction={"row"}
-						spacing={2}
-						justifyContent={"space-between"}
-					>
-						<Typography variant="h5">
-							{`Totale: `}
-							{totalPrice.toShow ? (
-								<Stack
-									display={"inline-flex"}
-									direction={"row"}
-									spacing={2}
-								>
-									<Typography
-										variant="h5"
-										textAlign={"center"}
-										color={"green"}
-									>
-										<strong>{totalPrice.totalDiscountedPrice}€</strong>
-									</Typography>
-									<Typography
-										variant="h5"
-										textAlign={"center"}
-										color={"grey"}
-										style={{
-											position: "relative",
-										}}
-									>
-										{totalPrice.totalPrice}€
-										<span
-											style={{
-												position: "absolute",
-												top: "50%",
-												left: "50%",
-												transform: "translate(-50%, -50%) rotate(-20deg)",
-												background: "red",
-												width: "100%",
-												height: "2px",
-											}}
-										></span>
-									</Typography>
-								</Stack>
-							) : (
-								<strong>{totalPrice.totalPrice}€</strong>
-							)}
-						</Typography>
-						{stripeSecretKey && (
-							<Button
-								variant="contained"
-								onClick={handleCheckout}
+										);
+
+										return (
+											<ListItem>
+												<ListItemText>
+													<Stack
+														alignItems={"center"}
+														spacing={2}
+														direction={isMobile ? "column" : "row"}
+													>
+														<Image
+															src={
+																abbonamento.immagine ? abbonamento.immagine : ""
+															}
+															alt={abbonamento.nome}
+															width={125}
+															height={125}
+															style={{ borderRadius: 5 }}
+														/>
+														<Stack spacing={2}>
+															<Box width={"200px"}>
+																<Typography variant="h6">
+																	{abbonamento.nome}
+																</Typography>
+																<Typography>
+																	{abbonamento.descrizione}
+																</Typography>
+															</Box>
+															{prices.discountedPrice ? (
+																<Stack
+																	direction={"row"}
+																	spacing={2}
+																>
+																	<Typography
+																		variant="h6"
+																		textAlign={"center"}
+																		color={"green"}
+																	>
+																		{prices.discountedPrice}€
+																	</Typography>
+																	<Typography
+																		variant="h6"
+																		textAlign={"center"}
+																		color={"grey"}
+																		style={{
+																			position: "relative",
+																		}}
+																	>
+																		{prices.basePrice}€
+																		<span
+																			style={{
+																				position: "absolute",
+																				top: "50%",
+																				left: "50%",
+																				transform:
+																					"translate(-50%, -50%) rotate(-20deg)",
+																				background: "red",
+																				width: "100%",
+																				height: "2px",
+																			}}
+																		></span>
+																	</Typography>
+																</Stack>
+															) : (
+																<Typography variant="h6">
+																	<strong>{prices.basePrice}€</strong>
+																</Typography>
+															)}
+															{isMobile ? sconti : <></>}
+														</Stack>
+														{!isMobile ? sconti : <></>}
+													</Stack>
+												</ListItemText>
+												<IconButton
+													edge="end"
+													aria-label="delete"
+													sx={{
+														marginRight: "auto",
+														color: theme.palette.error.main,
+													}}
+													onClick={() =>
+														removeFromCart(abbonamento, cart, dispatch)
+													}
+												>
+													<DeleteRounded />
+												</IconButton>
+											</ListItem>
+										);
+									})
+								) : (
+									<></>
+								)}
+							</List>
+							<Stack
+								direction={"row"}
+								spacing={2}
+								justifyContent={"space-between"}
 							>
-								CHECKOUT
-							</Button>
-						)}
-					</Stack>
+								<Typography variant="h5">
+									{`Totale: `}
+									{totalPrice.toShow ? (
+										<Stack
+											display={"inline-flex"}
+											direction={"row"}
+											spacing={2}
+										>
+											<Typography
+												variant="h5"
+												textAlign={"center"}
+												color={"green"}
+											>
+												<strong>{totalPrice.totalDiscountedPrice}€</strong>
+											</Typography>
+											<Typography
+												variant="h5"
+												textAlign={"center"}
+												color={"grey"}
+												style={{
+													position: "relative",
+												}}
+											>
+												{totalPrice.totalPrice}€
+												<span
+													style={{
+														position: "absolute",
+														top: "50%",
+														left: "50%",
+														transform: "translate(-50%, -50%) rotate(-20deg)",
+														background: "red",
+														width: "100%",
+														height: "2px",
+													}}
+												></span>
+											</Typography>
+										</Stack>
+									) : (
+										<strong>{totalPrice.totalPrice}€</strong>
+									)}
+								</Typography>
+								{stripeSecretKey && (
+									<Button
+										variant="contained"
+										onClick={handleCheckout}
+									>
+										CHECKOUT
+									</Button>
+								)}
+							</Stack>
+						</>
+					)}
 				</Container>
 			</Layout>
 		</ThemeProvider>
