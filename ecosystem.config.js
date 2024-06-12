f48d65224b252a3866feddbd302c8fe4150e99c3
Ubaldo Formichetti <info_uby@yahoo.it>
2024-06-12 14:54:45 +0200

    mod start propatria

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,12 +1,12 @@
 module.exports = {
 	apps: [
 		{
-			name: "ecommerce-byteware",
+			name: "ecommerce-propatria",
 			script: "node_modules/next/dist/bin/next",
 			args: "start",
 			env: {
 				NODE_ENV: "production",
-				PORT: 3000,
+				PORT: 3001,
 				HTTPS: true,
 			},
 		},
